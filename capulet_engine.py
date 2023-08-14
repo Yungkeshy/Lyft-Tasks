9a2bd6b685d6b99abefbadfcbbb1bd9408da70f7
Keshy <61046631+Yungkeshy@users.noreply.github.com>
2023-08-15 02:14:16 +0530

    Add files via upload

---
+++ capulet_engine.py
@@ -0,0 +1,13 @@
+from engine.engine import Engine
+
+
+
+#Creating Capulet Engine Class
+class CapuletEngine(Engine):
+    def __init__(self, current_mileage, last_service_mileage):
+        
+        self.current_mileage = current_mileage
+        self.last_service_mileage = last_service_mileage
+
+    def needs_service(self):
+        return self.current_mileage - self.last_service_mileage > 30000
